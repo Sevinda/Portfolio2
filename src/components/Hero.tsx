098416070fbe45c3ee7aa93ff1c6748adbf7044c
Sevinda Perera <sevindacharukaperera@gmail.com>
2023-12-07 11:53:29 +0530

    changed something

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div className="flex flex-col h-[calc(100vh-70px)] gap-[20px]">
       <h1 className="pt-[100px] text-[60px]">Sevinda Perera</h1>
-      <p className="text-[25px]">Software Engineering Undergraduate</p>
+      <p className="text-[25px]">Software Engineering Undergraduate shjbks</p>
     </div>
   );
 };
